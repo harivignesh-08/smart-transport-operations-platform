b20c1a93050dc3b02bd8bba14b115d28e5d40bd4
Gokila K <720824103049@hit.edu.in>
2026-07-12 11:45:50 +0530

    Created Digital Twin, Emergency and File services

---
+++ digital-twin-service/src/test/java/com/transitops/digital_twin_service/DigitalTwinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitops.digital_twin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
