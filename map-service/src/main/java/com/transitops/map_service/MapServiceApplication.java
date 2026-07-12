a98168fad24442434a5ae290bf34123b17b3d891
Gokila K <720824103049@hit.edu.in>
2026-07-12 16:34:06 +0530

    Completed Map Service CRUD implementation

--- map-service/src/main/java/com/transitops/map_service/MapServiceApplication.java
+++ map-service/src/main/java/com/transitops/map_service/MapServiceApplication.java
@@ -2,12 +2,14 @@ package com.transitops.map_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.transitops.map_service.repository")
 public class MapServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MapServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MapServiceApplication.class, args);
+    }
 
 }
